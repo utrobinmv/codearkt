97ae8b17136a243d80d256e4e1407c833c7922cc
Ilya Gusev <phoenixilya@gmail.com>
2025-07-22 18:44:29 +0000

    Simpler examples

--- examples/multi_agent/run.py
+++ examples/multi_agent/run.py
@@ -1,29 +1,69 @@
 import os
+from pathlib import Path
 
 from phoenix.otel import register
 from dotenv import load_dotenv
 
+from codearkt.codeact import CodeActAgent, Prompts
+from codearkt.llm import LLM
 from codearkt.server import run_server
 from codearkt.otel import CodeActInstrumentor
-from .agents import get_manager
 
 load_dotenv()
 
-MCP_CONFIG = {
-    "mcpServers": {"academia": {"url": "http://0.0.0.0:5056/mcp", "transport": "streamable-http"}}
-}
+current_dir = Path(__file__).parent
+
 PHOENIX_URL = os.getenv("PHOENIX_URL", "http://localhost:6006")
 PHOENIX_PROJECT_NAME = os.getenv("PHOENIX_PROJECT_NAME", "codearkt")
+EXA_API_KEY = os.getenv("EXA_API_KEY", "")
+MCP_CONFIG = {
+    "mcpServers": {
+        "academia": {"url": "http://0.0.0.0:5056/mcp", "transport": "streamable-http"},
+        "exa": {
+            "url": f"https://mcp.exa.ai/mcp?exaApiKey={EXA_API_KEY}",
+            "transport": "streamable-http",
+        },
+    }
+}
 
-register(
-    project_name=PHOENIX_PROJECT_NAME,
-    endpoint=f"{PHOENIX_URL}/v1/traces",
-    auto_instrument=True,
-)
-CodeActInstrumentor().instrument()
+LIBRARIAN_DESCRIPTION = """This team member runs gets and analyzes information from papers.
+He has access to ArXiv, Semantic Scholar, ACL Anthology, and web search.
+Ask him any questions about papers and web articles.
+Give him your task as an only string argument. Follow the task format described above, include all the details."""
+
+
+def get_librarian() -> CodeActAgent:
+    llm = LLM(model_name="deepseek/deepseek-chat-v3-0324")
+    prompts = Prompts.load(current_dir / "librarian.yaml")
+    return CodeActAgent(
+        name="librarian",
+        description=LIBRARIAN_DESCRIPTION,
+        llm=llm,
+        prompts=prompts,
+        tool_names=["academia_arxiv_download", "academia_arxiv_search", "exa_web_search_exa"],
+        planning_interval=5,
+    )
+
+
+def get_manager() -> CodeActAgent:
+    llm = LLM(model_name="deepseek/deepseek-chat-v3-0324")
+    return CodeActAgent(
+        name="manager",
+        description="A manager agent",
+        llm=llm,
+        managed_agents=[get_librarian()],
+        tool_names=[],
+        planning_interval=5,
+    )
 
 
 def main() -> None:
+    register(
+        project_name=PHOENIX_PROJECT_NAME,
+        endpoint=f"{PHOENIX_URL}/v1/traces",
+        auto_instrument=True,
+    )
+    CodeActInstrumentor().instrument()
     agent = get_manager()
     run_server(agent, MCP_CONFIG)
 
--- examples/simple_agent/run.py
+++ examples/simple_agent/run.py
@@ -1,10 +1,23 @@
-from codearkt.server import run_server
-from .agents import get_simple_agent
+from dotenv import load_dotenv
 
+from codearkt.server import run_server
+from codearkt.codeact import CodeActAgent
+from codearkt.llm import LLM
+
+load_dotenv()
 
 MCP_CONFIG = {
     "mcpServers": {"academia": {"url": "http://0.0.0.0:5056/mcp", "transport": "streamable-http"}}
 }
+
+
+def get_simple_agent() -> CodeActAgent:
+    return CodeActAgent(
+        name="manager",
+        description="A simple agent",
+        llm=LLM(model_name="deepseek/deepseek-chat-v3-0324"),
+        tool_names=["arxiv_download", "arxiv_search"],
+    )
 
 
 def main() -> None:
